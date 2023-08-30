5d23de1e64e0dbe32056af580f919e30078fa783
Larissa <73025368+larissafazza@users.noreply.github.com>
2023-08-30 11:40:28 -0300

    corret user seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class UserSeeder extends Seeder
             [
                 'name' => 'João',
                 'email' => 'joao@gmail.com',
-                'password' => '12345678',
+                'password' => Hash::make('12345678'),
             ],
         ]);
     }
